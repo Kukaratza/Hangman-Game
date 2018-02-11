78ee229ea19cd954d5830854f7d1a627d84ad6d4
Kukaratza <marioandres.martinez80@gmail.com>
2018-02-10 22:48:46 -0800

    working on indexOf

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -7,10 +7,13 @@ var winscount = 0;
 var losecount = 0;
 var underscore = [];
 
-//PC randomly generates a word from the array.           
-var randnum = Math.floor(Math.random() * randomwordarray.length); //picks random index from array and multiples and removes extra digits.  
-var randword = randomwordarray[randnum]; // the number picked (index) nows assigns it to the corresponding word in array.
-console.log(randword); //verify that pc it is selecting a random word.
+//PC randomly generates a word from the array. 
+//picks random index from array and multiples and removes extra digits.  
+var randnum = Math.floor(Math.random() * randomwordarray.length);
+// the number picked (index) nows assigns it to the corresponding word in array.
+var randword = randomwordarray[randnum];
+//verify that pc it is selecting a random word.
+console.log(randword);
 
 //create underscores based on the length of the word.
 var genunderscores = function () {
@@ -24,12 +27,19 @@ console.log(genunderscores());
 //THIS IS WHERE THE MAGIC BEGINS!!! 
 
 //Create a function the captures the key that user inputs.
-document.onkeyup = function (event) {
-    var userguess = event.key;
-    console.log(event);
-}
 
-//compare input from user with each letter from the word.
+//this fn starts after releasing the keypress
+document.onkeyup = function (event) {
+    var keycode = event.key;
+    //converts the "keycode" into the actual string letter.     
+    var userguess = String.fromCharCode(keycode);
+
+    //compare input from user with each letter from the randword and returns -1 if not found.
+    if(randword.indexOf(userguess) > -1) {
+        console.log(true);
+}
+    
+}
         //If letter is in the word, display it and show it.
         //if not set it to wrong guesses and deduct 1 point from "guesses left".
         //
